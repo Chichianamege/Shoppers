e83b3371bfe04f85a527c4d73f7dcc7fb9ba900d
jake <130408715+Chichianamege@users.noreply.github.com>
2024-05-15 21:49:18 -0400

    Add files via upload
    Write a program to simulate checkout lines at a grocery store. There will be multiple queues, one for each check out line. For this exercise, you may assume there are 5 check out lines. You can use an array of queues to simulate the checkout lines.
    
    Use the following logic:
    
    1.	Generate 5 customers and enqueue them, one customer in each queue
    
    2.	Each new customers ready to check out choose the shortest line
    
    Customers enter the check out queues randomly, and then each time a customer is generated that customer chooses the shortest line.
    
    If the lines are equal, then the first available line is chosen. Each transaction takes a random amount of time to complete. Print each action taken with Queue number, to the display.
    
    For your output show the queues with customers and activity, showing changes in each queue.

---
+++ dsassignment5parttwo.java
@@ -0,0 +1,60 @@
+// Name: Chidera Anamege
+// Class: CS 3305/W01
+// Term:Spring 2024
+//	Instructor:  Carla McManus
+//	Assignment:  05-Part-2-Shoppers
+
+
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class dsassignment5parttwo {
+    public static void main(String[] args) {
+        final int num_checkOutLineIndex = 5;
+        Queue<Integer>[] checkOutLineIndex = new ArrayDeque[num_checkOutLineIndex];
+        Random random = new Random();
+
+        // initialize checkout lines
+        for (int i = 0; i < num_checkOutLineIndex; i++) {
+            checkOutLineIndex[i] = new ArrayDeque<>();
+        }
+
+        // generate list of checkout line indices and shuffle them
+        List<Integer> checkout_line_indices = new ArrayList<>();
+        for (int i = 0; i < num_checkOutLineIndex; i++) {
+            checkout_line_indices.add(i);
+        }
+        Collections.shuffle(checkout_line_indices, random);
+
+        // generate customers and enqueue them in the shuffled checkout lines
+        for (int customer_id = 1; customer_id <= 5; customer_id++) {
+            int shortest_line = checkout_line_indices.get(customer_id - 1); // get the index from shuffled list
+            checkOutLineIndex[shortest_line].offer(customer_id);
+            System.out.println("customer " + customer_id + " entered queue " + shortest_line);
+            toSimulateTransaction();
+        }
+
+        // dequeue customers from all checkout lines in their original order
+        for (int customer_id = 1; customer_id <= 5; customer_id++) {
+            int shortest_line = checkout_line_indices.get(customer_id - 1); // get the index from shuffled list
+            if (!checkOutLineIndex[shortest_line].isEmpty()) {
+                int dequeued_customer_id = checkOutLineIndex[shortest_line].poll();
+                System.out.println("customer " + dequeued_customer_id + " exited queue " + shortest_line);
+            }
+        }
+    }
+
+    // simulate transaction time
+    private static void toSimulateTransaction() {
+        try {
+            Thread.sleep(new Random().nextInt(3000) + 1000); // random transaction time between 1 to 4 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
